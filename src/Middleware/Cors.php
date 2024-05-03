dd9e3fbd8fefbdfe3cadf84e973eb660d4a5a79a
Rodrigo Danieli <rodri.merlucci@gmail.com>
2024-05-03 12:17:00 -0300

    up

--- src/Middleware/Cors.php
+++ src/Middleware/Cors.php
@@ -29,7 +29,9 @@ class Cors
         $this->config = new Settings();
         $this->config
             ->setServerOrigin($exploded[0], $exploded2[0], $exploded2[1] ? $exploded2[1] : ($exploded[0] == "https" ? "443" : "80"))
-            ->setAllowedOrigins(is_array($system_config['allow_origin']) ? $system_config['allow_origin'] : [$system_config['allow_origin']])
+            ->enableAddAllowedHeadersToPreFlightResponse()
+            ->enableAddAllowedMethodsToPreFlightResponse()
+            ->enableAllOriginsAllowed()
             ->setAllowedMethods($system_config['allow_methods'])
             ->setAllowedHeaders($system_config['allow_headers'])
             ->setExposedHeaders($system_config['expose_headers'])
@@ -49,6 +51,8 @@ class Cors
     {
         $cors = $this->analyzer->analyze($request);
         switch ($cors->getRequestType()) {
+            case AnalysisResultInterface::TYPE_PRE_FLIGHT_REQUEST:
+                return new Response(200, $cors->getResponseHeaders());
             case AnalysisResultInterface::ERR_NO_HOST_HEADER:
                 return new Response(400, [], 'No host header present');
             case AnalysisResultInterface::ERR_ORIGIN_NOT_ALLOWED:
@@ -57,8 +61,6 @@ class Cors
                 return new Response(405, [], 'Method not supported');
             case AnalysisResultInterface::ERR_HEADERS_NOT_SUPPORTED:
                 return new Response(401, [], 'Headers not supported');
-            case AnalysisResultInterface::TYPE_PRE_FLIGHT_REQUEST:
-                return new Response(200, $cors->getResponseHeaders());
             case AnalysisResultInterface::TYPE_REQUEST_OUT_OF_CORS_SCOPE:
             default:
                 // call next middleware handler
